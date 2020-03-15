c22cb8035dce2eafc5577fb0329d1b3af6b6df34
Jumscrafteur <hsansane@gmail.com>
2020-03-15 13:59:11 +0100

    Ajout !setProf

---
+++ commandes/setProf.js
@@ -0,0 +1,9 @@
+﻿function setProf(message, arg) {
+  if (!message.member.hasPermission("ADMINISTRATOR")) return;
+  if (!message.guild.roles.cache.has(arg[0])) return message.channel.send('ID de role non valide');
+  let test = message.guild.roles.cache.get(arg[0]);
+  message.channel.send(`${test} est maintenant un grade prof`);
+  return test;
+}
+
+module.exports = setProf;
---
+++ index.js
@@ -0,0 +1,34 @@
+﻿require('dotenv').config();
+
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+const {
+  prefix
+} = require('./config.json');
+
+const setProf = require('./commandes/setProf')
+
+
+
+client.once('ready', () => {
+  console.log('Coro sensei ready!');
+});
+
+client.on('message', async message => {
+  let roleProf;
+  if (message.channel.type === "dm") return
+  if (!message.content.startsWith(prefix) || message.author.bot) return;
+
+  let messageArray = message.content.split(" ");
+  let commande = messageArray[0];
+  let args = messageArray.slice(1);
+
+  if (commande === `${prefix}setProf`) {
+    roleProf = setProf(message, args);
+  }
+
+  console.log(roleProf)
+});
+
+client.login(process.env.TOKEN);
